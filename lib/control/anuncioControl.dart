ce2334d1b9bc4fc3424936deae38a033ded96dc4
gerlane17 <gerlanefev2020@gmail.com>
2024-09-03 09:04:28 -0300

    atualizando pub

---
+++ lib/control/anuncioControl.dart
@@ -0,0 +1,41 @@
+import 'package:projeto/model/bancodeDados.dart';
+import 'package:projeto/model/anuncio.dart';
+
+class AnuncioController {
+  // Inicializando o banco de dados
+  final Bancodedados bancodados = Bancodedados();
+
+  // Método para adicionar um anúncio
+  Future<int> addAnuncio(Anuncio anuncio) async {
+    final db = await bancodados.database;
+    return await db.insert('anuncio', anuncio.toMap());
+  }
+
+  // Método para deletar um anúncio
+  Future<int> deletarAnuncio(int idAnuncio) async {
+    final db = await bancodados.database;
+    return await db
+        .delete('anuncio', where: 'id_anuncio = ?', whereArgs: [idAnuncio]);
+  }
+
+  // Método para editar um anúncio
+  Future<int> editarAnuncio(Anuncio anuncio) async {
+    final db = await bancodados.database;
+    return await db.update(
+      'anuncio',
+      anuncio.toMap(),
+      where: 'id_anuncio = ?',
+      whereArgs: [anuncio.idAnuncio],
+    );
+  }
+
+  // Método para listar todos os anúncios
+  Future<List<Anuncio>> getAnuncios() async {
+    final db = await bancodados.database;
+    final List<Map<String, dynamic>> listaDeAnuncios =
+        await db.query('anuncio');
+    return List.generate(listaDeAnuncios.length, (int i) {
+      return Anuncio.fromMap(listaDeAnuncios[i]);
+    });
+  }
+}
---
+++ lib/control/usuarioControl.dart
@@ -0,0 +1,56 @@
+//classe para listar, cadastro e atualizar usuarios
+
+import 'package:projeto/model/bancodeDados.dart';
+import 'package:projeto/model/usuarios.dart';
+
+class Usercontroller {
+//start no banco de dados, criando um objeto dele;
+//estrutura para criar objeto.
+  final Bancodedados bancodados = Bancodedados();
+
+  //metodo de cadastro fr usuario
+  //User é o nome da classe model de vocês
+  Future<int> addUser(Usuarios usuario) async {
+    //criando uma variavel de banco de dados para salvar os dados.
+    //serve para conectar o metodo ao banco de dados
+    final db = await bancodados.database;
+
+    //mandar os dados para o banco de dados
+    //user é o nome da tabela do banco de dados
+    return await db.insert('user', usuario.toMap());
+  } //fim do metodo de cadastro do usuário
+
+  //metodo de excluir usuarios
+  Future<int> deletarUsuario(int idUsuario) async {
+    //cria a variavel de banco de dados para acessar os metodos
+    final db = await bancodados.database;
+    //excluir o usuário dentro do banco de dados
+    return await db.delete('user', where: 'id=?', whereArgs: [idUsuario]);
+    /*delete o registro dentro da tabela user, onde id do usuário
+    for igual ao id passado no metodo deletarUsuario */
+  }
+
+  //metodo para editar o usuário
+  Future<int> editarUsuario(Usuarios usuarios) async {
+    //criar variavel de banco de dados acessar os metodos
+    final db = await bancodados.database;
+    //atualizar os dados do usuário no banco de dados
+    return await db.update('user', usuarios.toMap(),
+        where: "id=?", whereArgs: [usuarios.idUsuarios]);
+    /*atualize dentro da tabela user, os dados novos dos usuarios
+  onde id da tabela do usuário é igual id do usuário passado */
+  }
+
+//metodo de lista usuário
+  Future<List<Usuarios>> getUser() async {
+//criar variavel do banco de dados para usar os metodos
+    final db = await bancodados.database;
+    //criar variavel para receber a lista dos usuário de banco de dados
+    final List<Map<String, dynamic>> listadeusario = await db.query('user');
+    /*variavel do tipo lista, que recebe uma Map de String dinamicas 
+    que recebe todos os dados da tabela usuário do banco de dados */
+    return List.generate(listadeusario.length, (int i) {
+      return Usuarios.fromMap(listadeusario[i]);
+    });
+  }
+}//fim da classe UserController
